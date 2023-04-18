35ba4db2c50222fd18e748b7cd1990f44578085f
thiagoblanco99 <thiagoromero42@gmail.com>
2023-04-18 19:09:12 -0300

    ha

--- practica_2/ej_3/ej3.c
+++ practica_2/ej_3/ej3.c
@@ -12,9 +12,7 @@
 #include <signal.h>
 #include <string.h>
 
-int p_h[2];
-int h_p[2];
-
+int pfd[2];
 
 const char *msg = "abcdefghijklmnopqrstuvwxyz\n";
 
@@ -24,29 +22,23 @@ void catch_child(int sig_num, siginfo_t *psiginfo, void *pcontext)
     printf("Recibi signal #%d %s de %d, code: %d\n", sig_num,
             strsignal(sig_num), psiginfo->si_pid, psiginfo->si_code);
 }
+
 void padre(int pid)
 {
     int ret;
     int sz8 = strlen(msg);
-    char c;
     
     printf("hello from padre (%d), hijo es %d\n", getpid(), pid);
     
-    close(p_h[0]);
-    close(h_p[1]);
+    close(pfd[0]);
+    
     getchar();
     
-    ret = write(p_h[1], msg, sz8);
+    ret = write(pfd[1], msg, sz8);
     if( ret != sz8 )
         perror("write en el pipe");
     
-    close(p_h[1]);
-
-    while( read(h_p[0], &c, 1) )
-        {
-            putchar(c);
-        }   
-    close(h_p[0]);
+    close(pfd[1]);
 
     while( 1 )
         pause();
@@ -55,37 +47,17 @@ void padre(int pid)
 void hijo()
 {
     char c;
-    char salida[100]="";
-    int len = strlen(salida);
-
+    
     printf("hello from hijo, mi pid es %d, mi padre es %d\n", getpid(), getppid());
 
-    close(p_h[1]);
-    close(h_p[0]);
+    close(pfd[1]);
 
-    while( read(p_h[0], &c, 1) )
-        {
-            putchar(c);
-            salida[len+1] = '\0';
-            len = strlen(salida);
-            salida[len] = c-32;
-            
-        }
-    salida[len+1] = '\n';
-    close(p_h[0]);
+    while( read(pfd[0], &c, 1) )
+        putchar(c);
 
-    const char *mensaje = salida;
-    int ret;
-    int sz8 = strlen(mensaje);
-    
-    ret = write(h_p[1], mensaje, sz8);
-    if( ret != sz8 )
-        perror("write en el pipe");
-    
-    printf("\n%s\n",salida);
     printf("(%d) pipe closed, bye\n", getpid());
     
-    close(h_p[1]);
+    close(pfd[0]);
 }
 
 
@@ -101,14 +73,11 @@ int main(int argc, char *argv[])
     sigfillset(&act.sa_mask);
     sigaction(SIGCHLD, &act, NULL);
     
-    if( pipe(p_h) == -1 ) {
+    if( pipe(pfd) == -1 ) {
         perror("pipe");
         return 1;
     }
-    if( pipe(h_p) == -1 ) {
-        perror("pipe");
-        return 1;
-    }
+    
     pid = fork();
     
     switch( pid ) {
